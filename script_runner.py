121c983c1315c3c3996d3afb972a454ec98c0b7e
hadronnez <adrianordonezsanmartin@gmail.com>
2025-07-20 15:14:12 +0200

    run.py archive added

---
+++ script_runner.py
@@ -0,0 +1,6 @@
+
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    subprocess.run([sys.executable, '-m', 'scripts.combate_juego'])
--- scripts/combate_juego.py
+++ scripts/combate_juego.py
@@ -2,6 +2,7 @@
 from clases import *
 
 if __name__ == '__main__':
+    
     pikachu = Pokemon("Pikachu", 20, 10, 10, 10)
     charmander = Pokemon("Charmander", 20, 15, 5, 10)
 
