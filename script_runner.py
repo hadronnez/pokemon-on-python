ee2b700e9cc066e9b92ed58b6bb00c3cae65ac02
hadronnez <adrianordonezsanmartin@gmail.com>
2025-07-20 15:35:21 +0200

    script_runner improved

--- script_runner.py
+++ script_runner.py
@@ -1,6 +1,22 @@
 
 import subprocess
 import sys
+import os
 
 if __name__ == '__main__':
-    subprocess.run([sys.executable, '-m', 'scripts.combate_juego'])
+
+    folder = 'scripts'
+
+    while True:
+        filename = input("Escribe el nombre del archivo de scripts/ que quieres ejecutar (sin .py): ")
+
+        if os.path.join(folder, filename) == f'{folder}\{filename}':
+            subprocess.run([sys.executable, '-m', f'{folder}.{filename}'])
+        else:
+            print("No se ha encontrado el archivo")
+
+        if input("¿Quieres cerrar el script-runner? (s): ") == "s":
+            break 
+
+# El código tiene un error, si salta la excepción del os.path.join no hace el else de después. 
+# Por lo demás, el código funciona perfectamente
